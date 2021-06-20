cb9a9724187fb3635f7c365c0f8b594719a84eae
Your Name <you@example.com>
2021-06-19 21:05:49 -0300

    Segunda Entrega

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,23 +5,23 @@ export const Navbar = () => {
     return (
         <div>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="/">Navbar</a>
+  <a class="navbar-brand" href="/">E-commerce de Restaurante</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">Inicio <span class="sr-only"></span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/">Features</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="/">¿Quienes somos? <span class="sr-only"></span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/">Pricing</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Menú <span class="sr-only"></span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Contacto <span class="sr-only"></span></a>
       </li>
     </ul>
   </div>
