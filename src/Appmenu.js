ebeeba60a05d3554769d94f1e9c4450dd4cf762b
Your Name <you@example.com>
2021-06-26 21:51:38 -0300

    Tercera entrega

--- src/Appmenu.js
+++ src/Appmenu.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Navbar } from './components/Navbar'
 import { ItemListContainer } from './components/ItemListContainer'
+import {Productos} from './Products/products'
 
 export const Appmenu = () => {
   const greating = "Menú Mamma Mía!"
   return (
     <div>
-      <Navbar />
-      <ItemListContainer greating={greating} />
-
+ 
+    <Navbar />
+    <ItemListContainer greating={greating} />
+    <Productos/>
+   
     </div>
   )
 }
+
